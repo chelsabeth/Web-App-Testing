cc4696d9dddc9b33e760939b2c73e2f3b27f5886
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-12-05 18:32:25 -0500

    buttons in Dashboard.js

--- app-testing/src/components/Dashboard.js
+++ app-testing/src/components/Dashboard.js
@@ -1,1 +1,24 @@
+// should be updated when the user records activity on the `Dashboard` component.
 import React, { useState } from "react";
+
+const Dashboard = () => {
+  return (
+    <div className="buttons">
+      <div className="home-buttons">
+        <button>Orioles Strike</button>
+        <button>Orioles Ball</button>
+        <button>Orioles Foul</button>
+        <button>Orioles Hit</button>
+      </div>
+
+      <div className="away-buttons">
+        <button>Away Strike</button>
+        <button>Away Ball</button>
+        <button>Away Foul</button>
+        <button>Away Hit</button>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
--- app-testing/src/components/Display.js
+++ app-testing/src/components/Display.js
@@ -1,6 +1,9 @@
+// display the count of `balls` and `strikes` for the at-bat.
 import React, { useState } from "react";
 
 const Display = () => {
     const [count, setCount] = useState(0);
     const [countAway, setCountAway] = useState(0);
+
+
 }
